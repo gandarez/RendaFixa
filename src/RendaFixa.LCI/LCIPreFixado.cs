55760bc7ec5c854e3cbb0acced093c364032b7cd
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2017-03-31 14:42:24 -0300

    Adicionando bibliteca para cálculo de LCI

---
+++ src/RendaFixa.LCI/LCIPreFixado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gandarez.RendaFixa.LCI
+{
+    public class LCIPreFixado
+    {
+        public decimal Rentabilidade(decimal precoCompra, int meses, double taxa, bool truncate = true)
+        {
+            /*
+             *  .:: Fórmula ::.
+             *  
+             *  Rentabilidade=preCompra*((1+(taxa/100))^((meses/12)-1)*100)+1)
+             * 
+             */
+            var taxaConvertida = (decimal)(Math.Pow(1 + taxa / 100, (double)meses / 12) - 1);
+            var rentBruta = precoCompra  * (1 + taxaConvertida);
+            return truncate ? Math.Truncate(rentBruta * 100 * 10000) / 10000 : rentBruta;
+        }
+    }
+}
---
+++ tests/RendaFixa.LCI.Tests/LCIPreFixadoTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gandarez.RendaFixa.LCI;
+using Xunit;
+
+namespace RendaFixa.LCI.Tests
+{
+    public class LCIPreFixadoTest
+    {
+        [Fact]
+        public void Valor1000_Taxa14_3Meses_DeveRetornar_Rentabilidade_1035086_7806()
+        {
+            var lci = new LCIPreFixado();
+            var actual = lci.Rentabilidade(10000, 14, 3);
+
+            Assert.Equal(1035086.7806M, actual);
+        }
+    }
+}
